0e7a5cfa6284e02b6da4888da754cdcb14c28c06
sharat bhat <sharattbhat@gmail.com>
2022-03-26 09:38:03 +1100

    Display comment editor on button click

--- Models/DTOs/Comment/CommentDto.cs
+++ Models/DTOs/Comment/CommentDto.cs
@@ -1,6 +1,7 @@
 ﻿using Models.DTOs.UserProfile;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,9 +11,13 @@ namespace Models.DTOs.Comment
     public class CommentDto
     {
         public int Id { get; set; }
+        [Required]
         public int PostId { get; set; }
+        [Required]
         public string Content { get; set; }
+        [Required]
         public Guid CreatedBy { get; set; }
+        [Required]
         public DateTime CreatedOn { get; set; }
         public int Likes { get; set; }
         public UserProfileDto UserProfile { get; set; }
